95cb0d3de3f5317e72da12f259616aea89f688b4
Julija Sauleniene <julija.sauleniene@gmail.com>
2022-09-29 09:32:48 +0300

    solve Uzduotis_3

--- Uzduotis_3/main.py
+++ Uzduotis_3/main.py
@@ -5,9 +5,27 @@
 # * Nekeis žodyno, priimto kaip argumento, savo viduje.
 # * Kaip argumentą priims žodyną ir grąžins sąrašą su visomis jo reikšmėmis (values).
 
+from webbrowser import get
+
+
 audi = {
   "make": 'audi',
   "model": 'A6',
   "year": 2005,
   "color": 'white',
 }
+
+# funkcija "get_dict_values"
+def get_dict_values(all):
+  """Grąžina sąrašą su visomis jo reikšmėmis (values)"""
+
+  result = all.values()
+
+  return result
+
+################################ TEST ################################
+
+print(" Test: 1. funkcija 'get_dict_values'")
+
+values_list = get_dict_values(audi)
+print(values_list)
